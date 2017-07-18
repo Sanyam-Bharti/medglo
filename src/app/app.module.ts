f5c6abc0c878d52427e9681e3f76a1dad5d04c38
Sanyam Bharti <sanyam.bharti@synapse.com>
2017-07-18 18:53:50 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MaterialModule, MdDialogModule } from '@angular/material';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule, 
+    MdDialogModule,
+    ,FormsModule,
+     RouterModule.forRoot([
+      {
+        path: '',
+        component: LoginComponent
+      }])
   ],
   providers: [],
   bootstrap: [AppComponent]
