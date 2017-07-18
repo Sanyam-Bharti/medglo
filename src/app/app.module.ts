d79516d83afe21074b9a082a9f1fbc599aa8364f
Sanyam Bharti <sanyam.bharti@synapse.com>
2017-07-18 20:15:25 +0530

    login changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpModule } from '@angular/http';
 import {RouterModule} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MaterialModule, MdDialogModule } from '@angular/material';
 import { FormsModule } from "@angular/forms";
+import { LoginService } from "app/login/login.service";
 
 @NgModule({
   declarations: [
@@ -13,16 +16,19 @@ import { FormsModule } from "@angular/forms";
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     MaterialModule, 
     MdDialogModule,
-    ,FormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
      RouterModule.forRoot([
       {
         path: '',
         component: LoginComponent
-      }])
+      }
+    ])
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
